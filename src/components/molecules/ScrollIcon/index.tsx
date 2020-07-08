8f9ffa99d820ec1d991da3f72fce71ce01a88efc
atsss <atsushi0623itoh@gmail.com>
2020-07-08 15:38:15 -0600

    :sparkles: create ScrollIcon component

---
+++ src/components/molecules/ScrollIcon/index.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import { Frame } from "framer"
+import { useCycle } from "framer-motion"
+import scrollIcon from '../../../images/scrollIcon.png'
+import style from './style.module.sass'
+
+export const ScrollIcon = () => {
+  const [current, cycle] = useCycle("small", "big")
+
+  useEffect(() => {
+        const intervalId = setInterval(() => cycle(), 1000)
+        return () => clearInterval(intervalId)
+    });
+
+  return (
+    <Frame
+      variants={variants}
+      transition={transition}
+      initial={'small'}
+      animate={current}
+      radius={'100%'}
+    >
+      <img src={scrollIcon} className={style.image} alt='icon' />
+    </Frame>
+  )
+}
+
+const variants = {
+  small: {
+    width: 40,
+    height: 40,
+    y: 50,
+  },
+  big: {
+    width: 60,
+    height: 60,
+    y: 0,
+  },
+}
+const transition = {
+  type: "tween",
+  damping: 300,
+  duration: 1,
+}
