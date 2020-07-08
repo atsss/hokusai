9ab59acf172b6051ba31f2c23776d727902b7871
atsss <atsushi0623itoh@gmail.com>
2020-07-08 17:35:16 -0600

    :sparkles: create Txt component

---
+++ src/components/atoms/Txt/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import style from "./style.module.sass";
+
+interface Props {
+  tag?: keyof JSX.IntrinsicElements,
+  size?: string,
+  children: React.ReactNode
+}
+
+const txtFactory = (role: string) => ({
+  tag: Tag = "p",
+  size = "m",
+  children
+}: Props) => (
+  <Tag className={[style[role], style[size]].join(" ")} >
+    {children}
+  </Tag>
+);
+
+export const Txt = txtFactory("default");
+export const WhiteTxt = txtFactory("white");
