33170efc9523d11399c603c025187b4eda41ce92
atsss <atsushi0623itoh@gmail.com>
2020-07-09 00:57:16 -0600

    :sparkles: create FadeIn component

---
+++ src/components/atoms/FadeIn/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { motion, useViewportScroll } from "framer-motion";
+
+interface Props {
+  breakpoint: number,
+  children: React.ReactNode
+}
+
+export const FadeIn: React.FC<Props> = ({ breakpoint, children }) => {
+  const [isVisible, setVisible] = useState(false)
+  const { scrollYProgress } = useViewportScroll()
+
+  scrollYProgress.onChange(current => {
+    if(current > breakpoint) { setVisible(true) }
+    else { setVisible(false) }
+  })
+
+  return (
+    <motion.div
+      variants={variants}
+      transition={transition}
+      initial={isVisible ? "visible" : "hidden"}
+      animate={isVisible ? "visible" : "hidden"}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+const variants = {
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+  hidden: {
+    opacity: 0,
+    y: 100,
+  },
+}
+const transition = {
+  type: "spring",
+  damping: 300,
+  duration: 1,
+}
