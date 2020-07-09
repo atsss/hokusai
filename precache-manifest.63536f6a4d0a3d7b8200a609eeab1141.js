74daf52825265753c359c80e98909c398a983848
atsss <atsushi0623itoh@gmail.com>
2020-07-09 01:19:22 -0600

    Updates

---
+++ precache-manifest.63536f6a4d0a3d7b8200a609eeab1141.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24299319bfa9e09c917df31179eb534",
+    "url": "/hokusai/index.html"
+  },
+  {
+    "revision": "49f254c17de809705224",
+    "url": "/hokusai/static/css/2.a7bca8fd.chunk.css"
+  },
+  {
+    "revision": "c4082c708936e8c9ea7f",
+    "url": "/hokusai/static/css/main.c08172e2.chunk.css"
+  },
+  {
+    "revision": "49f254c17de809705224",
+    "url": "/hokusai/static/js/2.b1264120.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/hokusai/static/js/2.b1264120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4082c708936e8c9ea7f",
+    "url": "/hokusai/static/js/main.3cfd6311.chunk.js"
+  },
+  {
+    "revision": "faf4ef293d1c65375df4",
+    "url": "/hokusai/static/js/runtime-main.49392689.js"
+  },
+  {
+    "revision": "ed6475c9670f3d18fd5c4babfae2330a",
+    "url": "/hokusai/static/media/edo.ed6475c9.png"
+  },
+  {
+    "revision": "65ad841b8d50356d6c28d2d061af42f0",
+    "url": "/hokusai/static/media/great_wave.65ad841b.png"
+  },
+  {
+    "revision": "fb9685fb63dd138e9f722c9829a1a000",
+    "url": "/hokusai/static/media/portrait.fb9685fb.png"
+  },
+  {
+    "revision": "f1f966f077acd4335c99ee7613d137da",
+    "url": "/hokusai/static/media/print.f1f966f0.png"
+  },
+  {
+    "revision": "18df4309a2f10889c894ac43aecdd199",
+    "url": "/hokusai/static/media/prints.18df4309.png"
+  },
+  {
+    "revision": "1ee01f40714b3f208fe3fea9b3e845ed",
+    "url": "/hokusai/static/media/scrollIcon.1ee01f40.png"
+  },
+  {
+    "revision": "1c0d0de76846d6c46a84e99bee930766",
+    "url": "/hokusai/static/media/ukiyoes.1c0d0de7.png"
+  }
+]);
