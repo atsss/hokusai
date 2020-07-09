ce1a64f0ebe7f259b8b2981857dc0e35e6a3e827
atsss <atsushi0623itoh@gmail.com>
2020-07-08 19:44:25 -0600

    :sparkles: create Print component

--- src/components/pages/Top/index.tsx
+++ src/components/pages/Top/index.tsx
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react';
 import { AppTitle } from '../../organisms/AppTitle'
 import { Hokusai } from '../../organisms/tops/Hokusai'
 import { Ukiyoe } from '../../organisms/tops/Ukiyoe'
+import { Print } from '../../organisms/tops/Print'
 
 export const Top = () => (
   <Fragment>
     <AppTitle/>
     <Hokusai/>
     <Ukiyoe/>
+    <Print/>
   </Fragment>
 );
