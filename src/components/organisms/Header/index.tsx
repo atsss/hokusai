658118c51f4e6d0c235083092b4111c09013d19e
atsss <atsushi0623itoh@gmail.com>
2020-07-09 09:04:03 -0600

    :sparkles: create Header component

---
+++ src/components/organisms/Header/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { WhiteTxt } from '../../atoms/Txt';
+import { Img } from '../../atoms/Img';
+import style from './style.module.sass'
+import BackIcon from '../../../images/backIcon.png'
+
+interface Props {
+  back: string,
+  children: React.ReactNode,
+  className?: string
+}
+
+export const Header: React.FC<Props>  = ({ back, children, className }) => (
+  <header className={style.header}>
+    <div className={style.back}>
+      <Link to={back} className={[style.button, className].join(' ')}>
+        <Img src={BackIcon}/>
+      </Link>
+    </div>
+    <div className={style.title}>
+      <WhiteTxt size='l'>{children}</WhiteTxt>
+    </div>
+    <div className={style.dummy}>d</div>
+  </header>
+);
