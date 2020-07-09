69f16ab46c5dbb55d2c4dd97482310e28f5e5b57
atsss <atsushi0623itoh@gmail.com>
2020-07-08 18:36:59 -0600

    :recycle: refactor AppTitle component

--- src/components/organisms/AppTitle/index.tsx
+++ src/components/organisms/AppTitle/index.tsx
@@ -1,16 +1,21 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import style from './style.module.sass'
 import { ScrollIcon } from '../../molecules/ScrollIcon'
+import { Overlay } from '../../atoms/Overlay'
 import { WhiteTxt } from '../../atoms/Txt'
 
 export const AppTitle = () => (
-  <Fragment>
-    <div className={style.title}>
-      <WhiteTxt tag='h1' size='xxl'>Hokusai Katsushika</WhiteTxt>
-    </div>
-    <div className={style.icon}>
-      <ScrollIcon/>
-      <WhiteTxt size='s'>scroll up</WhiteTxt>
-    </div>
-  </Fragment>
+  <section className={style.container}>
+    <Overlay>
+      <div className={style.content}>
+        <div className={style.title}>
+          <WhiteTxt tag='h1' size='xxl'>Hokusai Katsushika</WhiteTxt>
+        </div>
+        <div className={style.icon}>
+          <ScrollIcon/>
+          <WhiteTxt size='s'>scroll up</WhiteTxt>
+        </div>
+      </div>
+    </Overlay>
+  </section>
 );
--- src/components/pages/Top/index.tsx
+++ src/components/pages/Top/index.tsx
@@ -1,14 +1,9 @@
-import React from 'react';
-import style from './style.module.sass'
-import { Overlay } from '../../atoms/Overlay'
+import React, { Fragment } from 'react';
 import { AppTitle } from '../../organisms/AppTitle'
 
 export const Top = () => (
-  <section className={style.container}>
-    <Overlay>
-      <div className={style.content}>
-        <AppTitle/>
-      </div>
-    </Overlay>
-  </section>
+  <Fragment>
+    <AppTitle/>
+    <AppTitle/>
+  </Fragment>
 );
