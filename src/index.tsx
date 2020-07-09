280811f4ac6438ab97d68a3853d8524c4f34e91b
atsss <atsushi0623itoh@gmail.com>
2020-07-09 17:24:37 -0600

    :bug: fix scroll position

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ScrollToTop } from './components/utils/ScrollToTop';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -8,6 +9,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
