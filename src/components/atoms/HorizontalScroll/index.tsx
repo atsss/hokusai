8de2732a07c645b0631f6f28b4f69d74b12ac5df
atsss <atsushi0623itoh@gmail.com>
2020-07-10 02:45:01 -0600

    :tada: improve HorizontalScroll

--- src/components/atoms/HorizontalScroll/index.tsx
+++ src/components/atoms/HorizontalScroll/index.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useElementScroll, useTransform } from "framer-motion";
 import style from './style.module.sass'
 
 interface Props {
   children: React.ReactNode[]
+  onChange?: () => void
 }
 
-export const HorizontalScroll: React.FC<Props> = ({ children }) => {
+export const HorizontalScroll: React.FC<Props> = ({ children, onChange = (i: number) => console.log(i) }) => {
+  const ref = useRef(null)
+  const { scrollXProgress } = useElementScroll(ref)
+  const index = useTransform(scrollXProgress, [0, 1], [0, children.length - 1])
+  let prevIndex = 0
+
+  index.onChange(current => {
+    const newIndex = Math.round(current)
+
+    if(newIndex !== prevIndex) { onChange(newIndex) }
+
+    prevIndex = newIndex
+  })
+
+
   return (
-    <ul className={style.gallery}>
+    <ul ref={ref} className={style.gallery}>
       {children!.map((child: React.ReactNode, index: number) => (
-        <li key={index} className={style.list}>{child}</li>
+        <li key={index} className={style.list}>
+          {child}
+        </li>
       ))}
     </ul>
   )
