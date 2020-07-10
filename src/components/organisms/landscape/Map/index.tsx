0b0432768de3060f5c60435c6e990b73509bcb88
atsss <atsushi0623itoh@gmail.com>
2020-07-10 00:03:22 -0600

    :lipstick: fix map ui

--- src/components/organisms/landscape/Map/index.tsx
+++ src/components/organisms/landscape/Map/index.tsx
@@ -1,24 +1,30 @@
 import React, { useState } from 'react';
+import style from './style.module.sass'
 import GoogleMapReact from 'google-map-react';
+import { Landscapes } from '../../../../config/Data'
+import PinImage from '../../../../images/pin.png';
+import { Img } from '../../../../components/atoms/Img';
 
-const AnyReactComponent = ({text}: any) => <div>{text}</div>;
+const defaultCenter = { lat: 35.2659257, lng: 138.9574245 }
+const Pin = (props: any) => <div className={style.pin}><Img src={PinImage}/></div>;
 
 export const Map = (props: any) => {
-    const [center, setCenter] = useState({lat: 11.0168, lng: 76.9558 });
-    const [zoom, setZoom] = useState(11);
+    const [index, setIndex] = useState(0)
+    const center = Landscapes[index]
+
     return (
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: "AIzaSyAOW1RNSrqQxStL842twRdvNkcyFIf_eEU" }}
-          defaultCenter={center}
-          defaultZoom={zoom}
+          defaultCenter={defaultCenter}
+          defaultZoom={7}
         >
-          <AnyReactComponent
-            lat={11.0168}
-            lng={76.9558}
-            text="My Marker"
+          <Pin
+            lat={center.lat}
+            lng={center.lng}
           />
         </GoogleMapReact>
+        <div style={{ height: '30px', width: '100%' }} onClick={() => setIndex(index+1)}/>
       </div>
     );
 }
--- src/config/Data/index.tsx
+++ src/config/Data/index.tsx
@@ -6,3 +6,12 @@ export const UkiyoeData = [
   { indexSrc: UkiyoeListOne, modalSrc: UkiyoeModal },
   { indexSrc: UkiyoeListTwo, modalSrc: UkiyoeModal },
 ]
+
+export const Landscapes = [
+ { lat: 35.4463437, lng: 139.7028493, name: 'Graet Wave', address: '20 Daikoku-Futo, Tsurumi-ku, Yokohama City, Kanagawa' },
+ { lat: 35.6812343, lng: 139.7723458, name: 'Nihonbashi', address: '1 Nihonbashi, Chuo-ku, Tokyo' },
+ { lat: 35.6117154, lng: 140.1047121, name: 'Nobutoura', address: '3-3-8 Noborito, Chuo-ku, Chiba-shi, Chiba' },
+ { lat: 35.2933914, lng: 139.2541174, name: 'Soshu Umezawa', address: '92-45 Ninomiya, Ninomiya-cho, Nakagun, Kanagawa' },
+ { lat: 35.5493868, lng: 138.8005565, name: 'Kaifeng Fast Sunny', address: 'Kawaguchi, Nishikatsura-cho, Minamitsuru-gun, Yamanashi' },
+ { lat: 35.1922165, lng: 138.0165699, name: 'Tomisantyu', address: 'Inuma, Kawane Honcho, Haibara-gun, Shizuoka' },
+]
