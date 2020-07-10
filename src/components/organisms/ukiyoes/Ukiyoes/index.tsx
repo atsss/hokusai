737a13403dbed1e8260b3110f711b32c0d5d1390
atsss <atsushi0623itoh@gmail.com>
2020-07-10 04:45:56 -0600

    :+1: fix data

--- src/components/organisms/ukiyoes/Ukiyoes/index.tsx
+++ src/components/organisms/ukiyoes/Ukiyoes/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { UkiyoeData } from "../../../../config/Data";
+import { EarlyUkiyoeData, MidUkiyoeData, LateUkiyoeData } from "../../../../config/Data";
 import { Section } from '../../../atoms/Section'
 import { FadeIn } from '../../../atoms/FadeIn'
 import { ImgCards } from "../../../molecules/ImgCards";
@@ -7,9 +7,9 @@ import { ImgCards } from "../../../molecules/ImgCards";
 export const Ukiyoes = () => (
   <Section>
     <FadeIn breakpoint={0.15}>
-      <ImgCards title='Early Edo period' data={UkiyoeData}/>
-      <ImgCards title='Middle Edo period' data={UkiyoeData} className='u-mt32'/>
-      <ImgCards title='Late Edo period' data={UkiyoeData} className='u-mt32'/>
+      <ImgCards title='Early Edo period' data={EarlyUkiyoeData}/>
+      <ImgCards title='Middle Edo period' data={MidUkiyoeData} className='u-mt32'/>
+      <ImgCards title='Late Edo period' data={LateUkiyoeData} className='u-mt32'/>
     </FadeIn>
   </Section>
 );
--- src/config/Data/index.tsx
+++ src/config/Data/index.tsx
@@ -1,17 +1,52 @@
 import UkiyoeListOne from "./../../images/ukiyoes/lists/one.png";
 import UkiyoeListTwo from "./../../images/ukiyoes/lists/two.png";
-import UkiyoeModal from "./../../images/ukiyoes/modals/popup.png";
+import UkiyoeListThree from "./../../images/ukiyoes/lists/three.png";
+import UkiyoeListFour from "./../../images/ukiyoes/lists/four.png";
+import UkiyoeListFive from "./../../images/ukiyoes/lists/five.png";
+import UkiyoeListSix from "./../../images/ukiyoes/lists/six.png";
+import UkiyoeListSeven from "./../../images/ukiyoes/lists/seven.png";
+import UkiyoeListEight from "./../../images/ukiyoes/lists/eight.png";
+import UkiyoeListNine from "./../../images/ukiyoes/lists/nine.png";
+import UkiyoeModalOne from "./../../images/ukiyoes/modals/one.png";
+import UkiyoeModalTwo from "./../../images/ukiyoes/modals/two.png";
+import UkiyoeModalThree from "./../../images/ukiyoes/modals/three.png";
+import UkiyoeModalFour from "./../../images/ukiyoes/modals/four.png";
+import UkiyoeModalFive from "./../../images/ukiyoes/modals/five.png";
+import UkiyoeModalSix from "./../../images/ukiyoes/modals/six.png";
+import UkiyoeModalSeven from "./../../images/ukiyoes/modals/seven.png";
+import UkiyoeModalEight from "./../../images/ukiyoes/modals/eight.png";
+import UkiyoeModalNine from "./../../images/ukiyoes/modals/nine.png";
+import LandscapeListOne from "./../../images/landscapes/lists/one.png";
+import LandscapeListTwo from "./../../images/landscapes/lists/two.png";
+import LandscapeListThree from "./../../images/landscapes/lists/three.png";
+import LandscapeListFour from "./../../images/landscapes/lists/four.png";
+import LandscapeListFive from "./../../images/landscapes/lists/five.png";
+import LandscapeModalOne from "./../../images/landscapes/modals/one.png";
+import LandscapeModalTwo from "./../../images/landscapes/modals/two.png";
+import LandscapeModalThree from "./../../images/landscapes/modals/three.png";
+import LandscapeModalFour from "./../../images/landscapes/modals/four.png";
+import LandscapeModalFive from "./../../images/landscapes/modals/five.png";
 
-export const UkiyoeData = [
-  { indexSrc: UkiyoeListOne, modalSrc: UkiyoeModal },
-  { indexSrc: UkiyoeListTwo, modalSrc: UkiyoeModal },
+export const EarlyUkiyoeData = [
+  { indexSrc: UkiyoeListOne, modalSrc: UkiyoeModalOne },
+  { indexSrc: UkiyoeListTwo, modalSrc: UkiyoeModalTwo },
+  { indexSrc: UkiyoeListThree, modalSrc: UkiyoeModalThree },
+]
+export const MidUkiyoeData = [
+  { indexSrc: UkiyoeListFour, modalSrc: UkiyoeModalFour },
+  { indexSrc: UkiyoeListFive, modalSrc: UkiyoeModalFive },
+  { indexSrc: UkiyoeListSix, modalSrc: UkiyoeModalSix },
+]
+export const LateUkiyoeData = [
+  { indexSrc: UkiyoeListSeven, modalSrc: UkiyoeModalSeven },
+  { indexSrc: UkiyoeListEight, modalSrc: UkiyoeModalEight },
+  { indexSrc: UkiyoeListNine, modalSrc: UkiyoeModalNine },
 ]
 
 export const Landscapes = [
- { lat: 35.4463437, lng: 139.7028493, name: 'Graet Wave', address: '20 Daikoku-Futo, Tsurumi-ku, Yokohama City, Kanagawa' },
- { lat: 35.6812343, lng: 139.7723458, name: 'Nihonbashi', address: '1 Nihonbashi, Chuo-ku, Tokyo' },
- { lat: 35.6117154, lng: 140.1047121, name: 'Nobutoura', address: '3-3-8 Noborito, Chuo-ku, Chiba-shi, Chiba' },
- { lat: 35.2933914, lng: 139.2541174, name: 'Soshu Umezawa', address: '92-45 Ninomiya, Ninomiya-cho, Nakagun, Kanagawa' },
- { lat: 35.5493868, lng: 138.8005565, name: 'Kaifeng Fast Sunny', address: 'Kawaguchi, Nishikatsura-cho, Minamitsuru-gun, Yamanashi' },
- { lat: 35.1922165, lng: 138.0165699, name: 'Tomisantyu', address: 'Inuma, Kawane Honcho, Haibara-gun, Shizuoka' },
+  { lat: 35.4463437, lng: 139.7028493, indexSrc: LandscapeListOne, modalSrc: LandscapeModalOne, name: 'Graet Wave', address: '20 Daikoku-Futo, Tsurumi-ku, Yokohama City, Kanagawa' },
+  { lat: 35.6812343, lng: 139.7723458, indexSrc: LandscapeListTwo, modalSrc: LandscapeModalTwo, name: 'Shinagawa gotenyama', address: '4-7 Kitashinagawa, Shinagawa-ku, Tokyo, 140-0001, Japan' },
+  { lat: 35.6117154, lng: 140.1047121, indexSrc: LandscapeListThree, modalSrc: LandscapeModalThree, name: 'Nobutoura', address: '3-3-8 Noborito, Chuo-ku, Chiba-shi, Chiba' },
+  { lat: 35.2933914, lng: 139.2541174, indexSrc: LandscapeListFour, modalSrc: LandscapeModalFour, name: 'Soshu Umezawa', address: '92-45 Ninomiya, Ninomiya-cho, Nakagun, Kanagawa' },
+  { lat: 35.1922165, lng: 138.0165699, indexSrc: LandscapeListFive, modalSrc: LandscapeModalFive, name: 'Tomisantyu', address: 'Inuma, Kawane Honcho, Haibara-gun, Shizuoka' },
 ]
